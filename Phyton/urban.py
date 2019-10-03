c9fb997a0744206e173c6cc5b89365618b544093
vyten <vyten@194.9.94.86>
2019-10-03 20:17:43 +0200

    Test Phyton

---
+++ Phyton/urban.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3.6m
+# coding: latin-1
+# ***************************************************************
+# File      : urban.py
+# Purpose   : learning python
+# Linked to :                  
+#                                
+# Updated by: Urban Björken
+# Update    : count correct answer
+# Date      : 2018-10-02
+#                        
+# Created by: Urban Björkén
+# Date      : 2018-10-02
+# email     : vyten@starnet.nu
+# ***************************************************************
+# ******************************************
+# clear linux screen
+# ******************************************
+from os import system
+system('clear')
+
+# ******************************************
+# variables (global ?)
+# ******************************************
+correctAnswer = 0
+totalNumbers = 0
+
+# ******************************************
+# import classes
+# ******************************************
+import urbanClasses
+
+# ***************************************************************
+# START SCRIPT
+# ***************************************************************
+# ******************************************
+# welcome
+# ******************************************
+print ("\nHej och Välkommen!")
+print ("Har kan du öva på multiplikationstabellen. Du kommer att få göra följande val:")
+print ("  * Vilken tabell (ex: 3)\n  * startposition (ex: 3)\n  * slutposition  (ex: 9)\n")
+
+# ******************************************
+# enduser input
+# ******************************************
+selectedTable = urbanClasses.Calculate.checkInteger(input('Skriv in tabell: '),'firstChoice')
+selectedLowerPosition = urbanClasses.Calculate.checkInteger(input('Skriv in start: '),'firstChoice')
+selectedHigherPosition = urbanClasses.Calculate.checkInteger(input('Skriv in stop: '),'firstChoice')
+
+# ******************************************
+# looping out questions
+# ******************************************
+for counter in range (selectedLowerPosition,(selectedHigherPosition+1),1):
+			# ******************************************
+			# calling Count class
+			# ******************************************
+			totalNumbers = urbanClasses.Calculate.counter(totalNumbers)						
+			answer = urbanClasses.Calculate.result(counter,selectedTable)
+			
+			# ******************************************
+			# if correct answer
+			# ******************************************
+			if answer == True:
+				correctAnswer = urbanClasses.Calculate.counter(correctAnswer)	
+
+# ******************************************
+# feedback to enduser
+# ******************************************
+print (correctAnswer ,"rätt av totalt" , totalNumbers , "tal")
+# ***************************************************************
+# END SCRIPT
+# ***************************************************************
---
+++ Phyton/urbanClasses.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3.6m
+# coding: latin-1
+# ***************************************************************
+# File      : urbanClasses.py
+# Purpose   : class in external file
+# Linked to : urban.py                 
+#                                
+# Updated by: Urban Björkén
+# Update    : result()
+#             return True if correct answer else return False
+# Date      : 2018-10-02 
+#                        
+# Created by: Urban Björkén
+# Date      : 2018-10-02
+# email     : vyten@starnet.nu
+# ***************************************************************
+class Calculate:
+	# ******************************************
+	# checkInteger()
+	# checks that it is an integer
+	# ******************************************
+	def checkInteger(cIs,cIv):
+		try:
+			cIi = int(cIs)
+		
+		except (ValueError) as eDesc:
+			# ******************************************
+			# exit script if it's first choice
+			# ******************************************
+			if cIv == 'firstChoice':
+				print ("Det måste vara heltal\nFel: [", eDesc, "]\n")
+				print("Script avslutat")
+				exit(0)
+			else:
+				print ("Det måste vara heltal")
+		else:
+			return cIi
+
+	# ******************************************
+	# result()
+	# Calculates and corrects answers from users
+	# printing answers correct | error
+	# ******************************************
+	def result(c,ui):
+		result = c * ui
+		print ("Vad är:", c, '*', ui,"?")
+	
+		# ******************************************
+		# calling class Validate
+		# ******************************************
+		userCountInput=Calculate.checkInteger(input('svar: '),'xxx')
+		if userCountInput == result:
+			print ("ratt\n")
+			return True
+		else:	
+			print ("fel, rätt svar är:",result ,"\n")
+			return False
+
+	# ******************************************
+	# counter()
+	# ******************************************
+	def counter(ci):
+		ci = ci + 1
+		return ci
